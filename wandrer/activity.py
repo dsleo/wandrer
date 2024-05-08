822acac279db5b7fbf553c936f0ab10f4e65cffd
dsleo <leooleds@gmail.com>
2024-05-08 20:55:21 +0200

    first code layout

---
+++ wandrer/activity.py
@@ -0,0 +1,103 @@
+import numpy as np
+from stravalib import Client
+from typing import Optional, List, Tuple, NamedTuple, Iterable
+from functools import partial
+from shapely.geometry import LineString
+from sklearn.neighbors import BallTree
+
+import sys
+sys.path.append('.')
+import itertools
+from utils import round_coordinates, sample_coords_by_distance, calculate_midpoint, segment_intersection, segment_length, complementary_region
+
+class HistoricalActivities():
+    def __init__(self, strava_client: Client, types: Optional[List[str]] = None):
+        self.strava_client = strava_client
+        self.types = types or ['latlng', 'distance']
+        self.path_segments = []
+        self.midpoints = []
+
+    def fetch_path_segments(self, sampling_interval: int = 500,limit: int = 10) -> Tuple[np.ndarray, np.ndarray]:
+        activities = list(itertools.islice(self.strava_client.get_activities(), limit))
+
+        existing_segments = set()
+        for activity in activities:
+            activity = Activity(strava_client=self.strava_client, activity_id=activity.id, types=self.types)
+            activity.fetch_path_segments(sampling_interval=sampling_interval)
+    
+            for segment in activity.segments:
+                segment_key = tuple(sorted(segment))
+                if segment_key not in existing_segments:
+                    self.path_segments.append(tuple(segment))
+                    existing_segments.add(segment_key)
+                    midpoint = calculate_midpoint(segment)
+                    self.midpoints.append(midpoint)
+
+        self.path_segments_np = np.array(self.path_segments)
+        self.midpoints_np= np.array([(mp[0], mp[1]) for mp in self.midpoints])
+
+    def index(self):
+        midpoints = np.array([[np.deg2rad(lat), np.deg2rad(lon)] for lat, lon in self.midpoints_np])
+        self.tree_index = BallTree(midpoints, metric='haversine')
+
+        return self.tree_index
+
+class Activity:
+    def __init__(self, strava_client: Client, activity_id, types: Optional[List[str]] = None):
+        self.strava_client = strava_client
+        self.activity_id = activity_id
+        self.types = types or ['latlng', 'distance']
+        self.path_segments = []
+        self.strava_segments = []
+
+    def fetch_strava_segments(self):
+        #WOULD BE NICE TO NOT do strava_client.client
+        activity_segments = self.strava_client.client.get_activity(self.activity_id, include_all_efforts="True").segment_efforts
+        for seg in activity_segments:
+            name = seg.name
+            start_latlng = seg.to_dict()['segment']['end_latlng']
+            end_latlng = seg.to_dict()['segment']['start_latlng']
+            self.strava_segments.append({"name": name, "start_latlng": start_latlng, "end_latlng": end_latlng})
+
+    def fetch_path_segments(self, sampling_interval=500):
+        #WOULD BE NICE TO NOT do strava_client.client
+        activity_data = self.strava_client.client.get_activity_streams(self.activity_id, types=self.types)
+        if 'latlng' in activity_data.keys():
+            coords=(activity_data['latlng'].data[:])
+            dists = (activity_data['distance'].data[:])
+            coords = round_coordinates(coords)
+            sampled_coords = sample_coords_by_distance(coords, dists, sampling_interval=sampling_interval)
+
+            self.path_segments = [(tuple(sampled_coords[i]), tuple(sampled_coords[i+1])) for i in range(len(sampled_coords)-1)]
+            self.midpoints = np.array([calculate_midpoint(segment) for segment in self.path_segments])
+
+    def get_new_path_segments(self, history: HistoricalActivities):
+        tree = history.tree_index
+        sampling_interval = 500
+        search_radius = sampling_interval / 6371e3
+
+        shared_segments = []
+        remainder_segments = []
+        test_list = np.array([[np.deg2rad(lat), np.deg2rad(lon)] for lat, lon in self.midpoints])
+
+        for idx, test_point in enumerate(test_list):
+            ind = tree.query_radius(test_point.reshape(1, -1), r=search_radius)
+            if ind[0].size>0:
+                full_intersect = []
+                for point in ind[0]:
+                    seg = history.path_segments_np[point]
+                    inter = segment_intersection(seg, self.path_segments[idx])
+                    inter_size = segment_length(inter)
+                    if inter_size > 0:
+                        shared_segments.append(inter)
+                        full_intersect.append(inter)
+                if len(full_intersect) > 0:
+                    remainder_seg = complementary_region(full_intersect, self.path_segments[idx])
+                    if len(remainder_seg) > 0:
+                        remainder_segments.append(remainder_seg)
+                    else:
+                        print("no more")
+        self.new_segments=remainder_segments
+        self.shared_segments=shared_segments 
+
+        return self.new_segments
---
+++ wandrer/utils.py
@@ -0,0 +1,72 @@
+import numpy as np
+import shapely.geometry as geometry
+from shapely import intersection
+from geopy.distance import geodesic
+
+def round_coordinates(coords, precision=3):
+    """Rounds a list of GPS coordinates."""
+    return [[round(x, ndigits=precision), round(y, ndigits=precision)] for x, y in coords]
+
+def sample_coords_by_distance(coords, dists, sampling_interval=500):
+    """Sample GPS coordinates based on cumulative distances.
+
+    Args:
+      coords: List of GPS coordinates in the form of (lat, lon) tuples.
+      dists: List of cumulative distances in meters corresponding to each GPS coordinate.
+      sampling_interval: Distance threshold in meters for selecting GPS coordinates.
+
+    Returns:
+      Sampled GPS coordinates in the form of a list of (lat, lon) tuples.
+    """
+    distances = np.array(dists)
+    current_distance = 0
+    indices = []
+    for i, dist in enumerate(distances):
+        if dist > current_distance + sampling_interval:
+            indices.append(i)
+            current_distance = dist
+    indices.append(len(distances)-1)
+    sampled_coords = [coords[idx] for idx in indices]
+    compression_ratio = len(coords) / len(sampled_coords)
+    #print("Number of sampled coordinates:", len(sampled_coords))
+    #print("Compression ratio:", round(compression_ratio, 2))
+    return sampled_coords
+
+def calculate_midpoint(segment):
+    """Calculates the midpoint between two GPS coordinate tuples."""
+    coord1 = segment[0]
+    coord2 = segment[1]
+
+    lat_mid = round((coord1[0] + coord2[0]) / 2., ndigits=3)
+    lon_mid = round((coord1[1] + coord2[1]) / 2., ndigits=3)
+    midpoint = (lat_mid, lon_mid)
+
+    return midpoint
+
+def segment_intersection(segment1, segment2):
+    line1 = geometry.LineString(segment1)
+    line2 = geometry.LineString(segment2)
+    inter = list(intersection(line1, line2).coords)
+    return inter
+
+def segment_length(segment):
+    if len(segment)>1:
+        return geodesic(segment[0], segment[1]).kilometers
+    else:
+        return 0
+
+def complementary_region(S1,  S2):
+    """Compute the complementary region of S1 in S2 using Shapely."""
+
+    united_lines = geometry.LineString(sum([list(x) for x in S1], []))
+    diff = geometry.LineString(S2).difference(united_lines)
+
+    if isinstance(diff, list):
+        return [list(geom.coords[0]) for geom in diff]
+    elif isinstance(diff, geometry.base.BaseGeometry):
+        if diff.is_empty:
+            return []
+        else:
+            return [list(diff.coords[0])]
+    else:
+        raise NotImplementedError("Unexpected type found during difference operation.")
